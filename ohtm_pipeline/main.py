a53d0185961e83fc6886123a3da78c65891789bb
PhilFAU <105867477+bayerschphi@users.noreply.github.com>
2025-12-03 15:01:01 +0100

    Revert "test"
    This reverts commit 14b67cad78ef18bda87228513a8776fc2692695a.

--- ohtm_pipeline/main.py
+++ ohtm_pipeline/main.py
@@ -13,7 +13,7 @@ First set the required paths.
 """
 
 import os
-from ohtm_pipeline.ohtm.pipeline import ohtm_pipeline_function
+from ohtm_pipeline.pipeline_function.pipeline import ohtm_pipeline_function
 
 ''' Path Settings: '''
 # Path to your mallet folder.
@@ -25,46 +25,42 @@ output_folder: str = (r"C:\Users\phili\sciebo - Bayerschmidt,"
                        r" Philipp (bayerschmidt@fernuni-hagen.de)@fernuni-hagen.sciebo.de\Topic Modeling\ohtm_files")
 
 # Set the path for your stop_word list.
-stopword_file_name = r"german_stopwords_custome.txt"
+stopword_file_name = r"german_stopwords_adoption.txt"
+# stopword_file_name = r"german_stopwords_custome.txt"
 
 # Set the path to your sources. This must be the folder, where your documents are stored in another folder.
-source_path: str = (
-    r"C:\Users\phili\sciebo - Bayerschmidt,"
-    r" Philipp (bayerschmidt@fernuni-hagen.de)@fernuni-hagen.sciebo.de\Interviews"
-    )
+source_path: str = r"C:\Users\phili\sciebo - Bayerschmidt, Philipp (bayerschmidt@fernuni-hagen.de)@fernuni-hagen.sciebo.de\Interviews"
 
-source_folder =  [
-    # 'Archiv Zwangsarbeit\komplett',
-    # 'Archiv Deutsches Gedächtnis\ADG_komplett',
-    # "Hannah Arendt Institut Dresden\Bereinigt",
-    # 'Flucht Vertreibung Versöhnung\Bereinigt',
-    # 'Museum Friedland\Bereinigt',
-    # 'Werkstatt der Erinnerungen\gesamt',
-    # "Colonia Dignidad\komplett_de",
-    'Infer\pal',
-    'Infer\who'
- ]
+source =  [
+    "Archiv Zwangsarbeit\komplett",
+    "Archiv Deutsches Gedächtnis\ADG_komplett", 
+    "Hannah Arendt Institut Dresden\Bereinigt",
+    "Flucht Vertreibung Versöhnung\Bereinigt",
+    "Museum Friedland\Bereinigt",
+    "Werkstatt der Erinnerungen\gesamt",
+    "Colonia Dignidad\komplett_de"
+    ]
 
 """ Topic Modeling Settings: """
 
-load_ohtm_file = False
-load_file_name = "jdh_ohtm_pipeline_short"
+ohtm_file_load_name = True
+load_file_name = "OHD_final_100c_100T_A5_final"
 
-create_ohtm_file = True
-save_ohtm_file = True
-save_name = "infer_test_2"
+create_ohtm_file = False
+save_ohtm_file = False
+ohtm_file_save_name = "jdh_inferred_"
 
 # You need to set a save_name and set the option save_json to True to save the model
 save_model = False
 
-use_preprocessing = True
+use_preprocessing = False
 
 # If you don't want to chunk your documents, set use_chunking to True and chunk_setting to 0
-use_chunking = True
+use_chunking = False
 chunk_setting = 100
 
-use_topic_modeling = True
-topics = 100
+use_topic_modeling = False
+topics = 45
 
 
 save_topic_words = False
@@ -72,21 +68,21 @@ print_topic_words = False
 number_of_words = 100
 
 print_ohtm_file = False
-print_ohtm_file_settings = False
-show_bar_graph_corpus = False
-show_heatmap_corpus = True
+print_ohtm_file_settings = True
+show_bar_graph_corpus = True
+show_heatmap_corpus = False
 
 z_score_filter = False
-filter_heatmap_corpus = True
+filter_heatmap_corpus = False
 archive_selected = "MFL"
 
 interview_id = "MFL20006"
-chunk_number = 1
+chunk_number = 23
 show_heatmap_interview = False
-print_interview_chunk = True
+print_interview_chunk = False
 search_for_topics_in_chunks = False
-topic_search = 44
-chunk_weight = 0.4
+topic_search = 93   
+chunk_weight = 0.01
 search_for_topics_in_interview = False
 
 ''' advanced options: '''
@@ -106,12 +102,13 @@ folder_as_archive = False
 stopword_removal_by_stop_list = True
 stopword_removal_by_spacy = False
 
-use_lemmatization = False
+use_lemmatization = True
 lemmatization_model_spacy = "de_core_news_lg"
-use_pos_filter = False
+use_pos_filter = True
 # possible settings: 'NOUN', 'PROPN', 'VERB', 'ADJ', 'ADV', 'PRON', 'ADP', 'DET', 'AUX', 'NUM', 'SCONJ', 'CCONJ', 'X'
 allowed_postags_settings_lemmatization = ['NOUN', 'PROPN', 'VERB', 'ADJ', 'NUM', 'ADV']
 
+# Anonymization settings
 anonymize = False
 # List of interviews, that should not be anonymized
 exceptions = []
@@ -126,18 +123,18 @@ topic_words_with_weight = True
 ''' Inferring new documents with an trained topic model'''
 
 infer_new_documents = False
-trained_ohtm_file = "OHD_final_100c_100T_A5_remade"  # load the trained json and the model to train that json
+trained_ohtm_file = "jdh_ohtm_pipeline_final_version"  # load the trained json and the model to train that json
 save_separate_ohtm_file = True  # save the inferred documents as a new json
-separate_ohtm_file_name = "ins_inferred"
+separate_ohtm_file_name = "inferred"
 
 if __name__ == "__main__":
     ohtm_pipeline_function(
         output_folder=output_folder,
-        source_folder=source_folder,
+        source_folder=source,
         source_path=source_path,
         stopword_file_name=stopword_file_name,
         allowed_postags_settings=allowed_postags_settings_lemmatization,
-        save_name=save_name,
+        save_name=ohtm_file_save_name,
         load_file_name=load_file_name,
         mallet_path= mallet_path,
         interview_id=interview_id,
@@ -153,7 +150,7 @@ if __name__ == "__main__":
         random_seed=random_seed,
         save_ohtm_file=save_ohtm_file,
         create_ohtm_file=create_ohtm_file,
-        load_ohtm_file=load_ohtm_file,
+        load_ohtm_file=ohtm_file_load_name,
         use_preprocessing=use_preprocessing,
         use_chunking=use_chunking,
         use_topic_modeling=use_topic_modeling,
